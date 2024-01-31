dc6e5ec3521811b822374b01279498fda17b7b1d
Jōji <gpgerov+github@gmail.com>
2024-01-31 03:48:06 +0100

    implemented basic player actions
    the transaction system does not work properly currently

--- bj_logic.js
+++ bj_logic.js
@@ -65,9 +65,9 @@ class Dealer {
 class Table {
     constructor(_dealer) {
         this.dealer = _dealer;
-        this.gameRunning = false;
         this.playerCount = prompt("Player count: ", '');;
         this.players = [];
+        this.currentPlayer = 0;
         this.#createPlayers();
     }
 
@@ -102,8 +102,10 @@ class Table {
 
         let credit = document.createElement("p");
 
-        _player.handTag = hand;
         _player.mainTag = main;
+        _player.handTag = hand;
+        _player.creditTag = credit;
+        _player.betTag = bet;
 
         main.classList.add("player");
         main.id = _player.id;
@@ -140,30 +142,48 @@ class Table {
         btn_hold.innerHTML = "hold";
         btn_hold.classList.add("button");
         btn_hold.addEventListener("click", () => {
-
+            if (_player.awaitAction) {
+                main.classList.remove("player-active");
+                _player.awaitAction = false;
+                this.#nextPlayer();
+            }
         });
 
         btn_give.innerHTML = "give";
         btn_give.classList.add("button");
         btn_give.addEventListener("click", () => {
-            _player.isReady = true;
-            let counter = 0;
-            for (let player of this.players) {
-                if (player.isReady) {
-                    counter++;
+            if (!_player.isReady) {
+                _player.isReady = true;
+                let counter = 0;
+                for (let player of this.players) {
+                    if (player.isReady) {
+                        counter++;
+                    }
                 }
-            }
-            if (counter === this.players.length) {
-                // START GAME HERE
-                this.gameRunning = true;
-                this.#initGame();
+                if (counter === this.players.length) {
+                    // START GAME HERE
+                    this.gameRunning = true;
+                    this.#initGame();
+                }
             }
         });
 
         btn_take.innerHTML = "take";
         btn_take.classList.add("button");
         btn_take.addEventListener("click", () => {
+            if (_player.awaitAction) {
+                _player.hand[0].cards.push(this.dealer.dealCard());
 
+                _player.handTag.innerHTML = _player.hand[0].getValue() + "\t" + _player.hand[0].getAllCards();
+
+                if (_player.hand[0].getNumericValue() > 21) {
+                    main.classList.remove("player-active");
+                    main.classList.add("player-loose")
+                    _player.awaitAction = false;
+
+                    this.#nextPlayer();
+                }
+            }
         });
 
         coins.classList.add("buttons");
@@ -175,7 +195,8 @@ class Table {
         coin50.classList.add("coin");
         coin50.addEventListener("click", () => {
             if (!_player.isReady) {
-                this.#updateCreditAndBet(_player, 50, bet, credit);
+                this.#updateCreditAndBet(_player, 50);
+                this.#updateCreditAndBetUI(_player);
             }
         });
 
@@ -183,7 +204,8 @@ class Table {
         coin100.classList.add("coin");
         coin100.addEventListener("click", () => {
             if (!_player.isReady) {
-                this.#updateCreditAndBet(_player, 100, bet, credit)
+                this.#updateCreditAndBet(_player, 100);
+                this.#updateCreditAndBetUI(_player)
             }
         });
 
@@ -191,7 +213,8 @@ class Table {
         coin500.classList.add("coin");
         coin500.addEventListener("click", () => {
             if (!_player.isReady) {
-                this.#updateCreditAndBet(_player, 500, bet, credit)
+                this.#updateCreditAndBet(_player, 500);
+                this.#updateCreditAndBetUI(_player);
             }
         });
 
@@ -201,22 +224,25 @@ class Table {
         return main;
     }
 
-    #updateCreditAndBet(_player, _amount, _betTag, _creditTag) {
+    #updateCreditAndBet(_player, _amount) {
         _player.credit -= _amount;
         _player.bet += _amount;
-        _betTag.innerHTML = _player.bet;
-        _creditTag.innerHTML = _player.credit;
+    }
+    #updateCreditAndBetUI(_player) {
+        _player.betTag.innerHTML = _player.bet;
+        _player.creditTag.innerHTML = _player.credit;
     }
 
     #initGame() {
-        let timeoutVal = 1000;
+        let timeoutVal = 800;
 
         this.#dealCards();
         console.log(this)
         this.#displayDealtCards(timeoutVal);
 
         setTimeout(() => {
-            let firstPlayer = this.players[this.players.length - 1];
+            this.currentPlayer = this.players.length - 1;
+            let firstPlayer = this.players[this.currentPlayer];
             firstPlayer.mainTag.classList.add("player-active");
             firstPlayer.awaitAction = true;
         }, timeoutVal * this.players.length * 2 + timeoutVal * 4)
@@ -263,6 +289,90 @@ class Table {
             this.dealer.handTag.innerHTML = this.dealer.hand.getFirstCardValue() + "\t" + this.dealer.hand.getFirstCardWithPlaceholder();
         }, _timeoutVal * this.players.length * 2 + _timeoutVal * 3);
     }
+
+    #nextPlayer() {
+        this.currentPlayer--;
+        if (this.currentPlayer !== -1) {
+            let nextPlayer = this.players[this.currentPlayer];
+            nextPlayer.mainTag.classList.add("player-active");
+            nextPlayer.awaitAction = true;
+        } else {
+            let index = 0;
+            let timeout = 800;
+
+            this.dealer.handTag.innerHTML = this.dealer.hand.getNumericValue() + "\t" + this.dealer.hand.getAllCards();
+
+            const dealDealerHand = () => {
+                if (this.dealer.hand.getNumericValue() < 17) {
+                    setTimeout(() => {
+                        this.dealer.hand.cards.push(this.dealer.dealCard());
+                        this.dealer.handTag.innerHTML = this.dealer.hand.getNumericValue() + "\t" + this.dealer.hand.getAllCards();
+                        dealDealerHand();
+                    }, timeout)
+                }
+            }
+
+            dealDealerHand();
+
+            setTimeout(() => {
+                this.#payMoney();
+            }, timeout * 8);
+
+            setTimeout(() => {
+                this.#resetTable();
+            }, timeout * 10);
+        }
+    }
+
+    // TODO: Ausschüttung funktioniert noch nicht korrekt!
+    #payMoney() {
+        for (let player of this.players) {
+            let playerValue = player.hand[0].getNumericValue();
+            let dealerValue = this.dealer.hand.getNumericValue();
+
+            const basicCheck = () => {
+                if (playerValue === 21 && player.hand[0].cards.length === 2) {
+                    alert("Player " + player.name + " got a BLACK JACK!");
+                    player.credit += player.bet * 2.5;
+                } else if (playerValue <= 21) {
+                    player.credit += player.bet * 2;
+                }
+            }
+
+            if (dealerValue > 21 ) {
+                basicCheck();
+            } else if (dealerValue <= 21 && this.dealer.hand.cards.length) {
+                if (playerValue > dealerValue) {
+                    basicCheck();
+                } else if (playerValue === dealerValue) {
+                    player.credit += player.bet;
+                }
+            } else if (dealerValue === 21 && this.dealer.hand.cards.length === 2) {
+                if (playerValue === 21 && player.hand[0].length === 2) {
+                    player.credit += player.bet;
+                }
+            }
+            player.bet = 0;
+            this.#updateCreditAndBetUI(player);
+        }
+    }
+
+    #resetTable() {
+        this.dealer.hand.cards = [];
+        this.dealer.handTag.innerHTML = "";
+
+        for (let player of this.players) {
+            player.handTag.innerHTML = "";
+            player.hand[0].cards = [];
+            try {
+                player.mainTag.classList.remove("player-active");
+            } catch (ignored) {}
+            try {
+                player.mainTag.classList.remove("player-loose");
+            } catch (ignored) {}
+            player.isReady = false;
+        }
+    }
 }
 
 class Player {
@@ -272,9 +382,11 @@ class Player {
         this.bet = 0;
         this.name = _name;
         this.hand = [];
-        this.hand.push(new Hand())
-        this.handTag = null;
+        this.hand.push(new Hand());
         this.mainTag = null;
+        this.handTag = null;
+        this.creditTag = null;
+        this.betTag = null;
         this.isReady = false;
         this.awaitAction = false;
     }
@@ -294,6 +406,9 @@ class Hand {
     }
 
     getFirstCardValue() {
+        if (this.cards[0].value === 1) {
+            return "<mark class='size-value'>" + 11 + "</mark>";
+        }
         return "<mark class='size-value'>" + this.cards[0].value + "</mark>";
     }
 
@@ -307,6 +422,18 @@ class Hand {
             }
         }
         return "<mark class='size-value'>" + value + "</mark>";
+    }
+
+    getNumericValue() {
+        let value = 0
+        for (let card of this.cards) {
+            if (card.value === 1 && value <= 10) {
+                value += 11;
+            } else {
+                value += card.value;
+            }
+        }
+        return value;
     }
 
     getFirstCard() {
